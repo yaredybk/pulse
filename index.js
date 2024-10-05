8bc442bc762518670787270aee3ad4bc48e93582
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-10-05 20:58:08 +0300

    feature: load react at /a

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const WebSocket = require('ws');
 const { newWebSocket } = require('./utils/webSocket');
 const app = express();
 
+app.use('/a', express.static('a', { fallthrough: false }));
 app.get('/', (rq, rs) => {
   rs.send(`<h1>PULSE</h1>
-    <p>this is a chat application</p>`);
+<p>a chat application</p>
+<a href="/a">start a chat</a>  
+`);
 });
 
 const server = app.listen(process.env.PORT || 5000, () => {
